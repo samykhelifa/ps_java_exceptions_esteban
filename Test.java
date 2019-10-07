c72fe49bd4e920326644b44f3932a0c85c7fe6e6
samy <39716963+samykhelifa@users.noreply.github.com>
2019-10-07 09:08:33 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,1 +1,24 @@
 
+/**
+ * Common exception methods
+ *
+ */
+public class Test
+{
+    public static void main( String[] args )
+    {
+        try {
+            throw new Exception("Sample exception");
+        } catch(Exception e) {
+            System.err.println("getMessage(): " + e.getMessage());
+            System.err.println("getLocalizedMessage(): " + e.getLocalizedMessage());
+            System.err.println("getCause(): " + e.getCause());
+            System.err.println("printStackTrace(): ");
+            e.printStackTrace();
+            System.err.println("fillInStackTrace(): ");
+            e.fillInStackTrace();
+            e.printStackTrace();
+
+        }
+    }
+}
